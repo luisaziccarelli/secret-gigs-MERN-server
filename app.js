10449d9f33cbf67d7a6f6f5ec3a441dd66768382
Ignacio Giadach <igiadach@gmail.com>
2020-07-10 10:51:56 +1000

    added structure, db Schema and event model
    Co-authored-by: Luisa Ziccarelli <luisaziccarelli@gmail.com>

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose")
 
 // Sets port if deploying to external provider 
 // or port assigned already
@@ -12,6 +13,25 @@ const app = express();
 // Call the middleware we want to use
 app.use(cors());
 app.use(bodyParser.json());
+
+// Connect to database
+const dbConn = "mongodb://localhost/secret_gigs"
+
+mongoose.connect(
+    dbConn,
+    {
+        useNewUrlParser : true,
+        useUnifiedTopology : true,
+        useFindAndModify : false
+    }, 
+    err => {
+        if (err){
+            console.log("error connecting database", err)
+        } else{
+            console.log("Connected to SG Database!")
+        }
+    }
+)
 
 // Define a simple route for GET
 app.get("/",(req,res) => {
