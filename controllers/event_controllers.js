c7a492644e015d4e351d0558e3b43255677d47a1
Ignacio Giadach <igiadach@gmail.com>
2020-07-11 10:34:47 +1000

    CRUD operations working. Utils, controller and routes.

--- controllers/event_controllers.js
+++ controllers/event_controllers.js
@@ -1,4 +1,4 @@
-const {getAllEvents, addEvent} = require("../utils/event_utilities")
+const {getAllEvents, addEvent, getEventById, updateEvent, deleteEvent} = require("../utils/event_utilities")
 
 const getEvents = function(req,res){
     getAllEvents().exec((err,events) => {
@@ -27,4 +27,40 @@ const postEvent = function(req,res){
     )
 }
 
-module.exports = {getEvents, postEvent}
+const getEvent = function(req,res){
+    getEventById(req.params.id).exec((err,event) => {
+        if (err){
+            res.status(404)
+            return res.send("Event not found")
+        }
+        res.send(event)
+    })
+}
+
+const modifyEvent = function(req, res){
+    updateEvent(req).exec((err,event)=>{
+        if (err){
+            res.status(500)
+            return res.json({
+                error: err.message
+            })
+        }
+        res.status(200)
+        res.send(event)
+    }
+    )
+}
+
+const removeEvent = function(req,res){
+    deleteEvent(req.params.id).exec((err,event)=>{
+        if (err){
+            res.status(500)
+            return res.json({
+                error: err.message
+            })
+        }
+        res.sendStatus(204)
+    })
+}
+
+module.exports = {getEvents, postEvent, getEvent, modifyEvent, removeEvent}
--- utils/event_utilities.js
+++ utils/event_utilities.js
@@ -8,4 +8,18 @@ const addEvent = function(body){
     return new Event(body)
 }
 
-module.exports = {getAllEvents, addEvent}
+const getEventById = function(id){
+    return Event.findById(id)
+}
+
+const updateEvent = function(req){
+    return Event.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    })
+}
+
+const deleteEvent = function(id){
+    return Event.findByIdAndRemove(id)
+}
+
+module.exports = {getAllEvents, addEvent, getEventById, updateEvent, deleteEvent}
