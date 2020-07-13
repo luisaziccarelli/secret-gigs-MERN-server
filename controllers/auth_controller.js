7a8cbadd2c0d20bb5755d254e50c65138bb8c9e0
LuisaZ <luisaziccarelli@gmail.com>
2020-07-13 22:57:36 +1000

    created login and logout functions, tested ok in postman
    Co-authored-by: Ignacio Giadach <igiadach@gmail.com>

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -2,6 +2,24 @@ const passport = require("passport")
 const User = require("../models/user")
 
 //register a user
+// const register = function (req, res) {
+//     User.register(new User({
+//         username: req.body.username,
+//         email: req.body.email,
+//         phoneNumber: req.body.phoneNumber
+//     }), req.body.password, function (err) {
+//         if (err) {
+//             res.status(500)
+//             res.json({error: err})
+//         } else {
+//             // Log in the newly registered user
+//             passport.authenticate('local')(req, res, function () {
+//                 res.json(req.user)
+//             })
+//         }
+//     })
+// }
+
 const register = function (req, res) {
     User.register(new User({
         username: req.body.username,
@@ -12,12 +30,32 @@ const register = function (req, res) {
             res.status(500)
             res.json({error: err})
         } else {
-            // Log in the newly registered user
-            passport.authenticate('local')(req, res, function () {
-                res.json(req.user)
-            })
+            loginUser(req, res)
         }
     })
 }
 
-module.exports = { register }
+
+const authenticate = passport.authenticate("local")
+// helper function
+
+function loginUser(req, res) {
+    	// passport.authenticate returns a function that we will call 
+//with req, res, and a callback function to execute on success    
+
+    authenticate(req, res, function () {
+        res.json(req.user)
+    })
+}
+
+
+const logout = function(req, res) {
+	req.logout()
+	console.log("logged out user")
+	console.log("session object:", req.session)
+	console.log("req.user:", req.user)
+	res.sendStatus(200)
+}
+
+
+module.exports = { register, loginUser, logout }
