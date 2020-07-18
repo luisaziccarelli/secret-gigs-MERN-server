7a181b90155b55bd24f91f12fdf801a4268610de
Ignacio Giadach <igiadach@gmail.com>
2020-07-19 00:49:17 +1000

    event utilities updateApplyToEvent applies to event only once

--- utils/event_utilities.js
+++ utils/event_utilities.js
@@ -1,4 +1,7 @@
 const Event = require("../models/event")
+const User = require("../models/user")
+const { ObjectID } = require('mongodb');
+
 
 const getAllEvents = function(req){
     return Event.find()
@@ -23,18 +26,41 @@ const deleteEvent = function(req){
 }
 
 const updateApplyToEvent = async (req) => {
-    let event = await Event.findById(req.params.id);
-console.log(req.body)
-console.log("THIS IS THE EVENT",event)
-    let newApplication = {
-        username: req.body.username,
-        accepted: false
-    }
-    event.applicants.push(newApplication);
-    return Event.findByIdAndUpdate(req.params.id, event, {
-        new: true
+    let event = await Event.findById(req.params.id)
+    let user = await User.find({
+        "username":`${req.body.username}`
     })
+
+    let foundMatches = await Event.find({
+        "_id": ObjectID(`${event.id}`),
+        "applicants": { "username": `${req.body.username}`, "accepted": false }
+    })
+
+    if (foundMatches[0] === undefined && user[0] !== undefined) {
+        let newApplication = {
+            username: req.body.username,
+            accepted: false
+        }
+        let newEventAppliedTo = {
+            eventId: event.id
+        }
+        event.applicants.push(newApplication)
+        user[0].eventsApplied.push(newEventAppliedTo)
+
+        return Event.findByIdAndUpdate(req.params.id, event, {
+            new: true
+        })
+        // return User.findByIdAndUpdate(user.id, user, {
+        //     new: true
+        // })
+
+    } else {
+        return Event.findById(req.params.id)
+    }
+
 }
+
+
 
 module.exports = {
     getAllEvents, 
