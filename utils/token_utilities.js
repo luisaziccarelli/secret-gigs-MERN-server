dd4f5c681f993345b7808956a00d13a5fc612b84
Ignacio Giadach <igiadach@gmail.com>
2020-07-22 23:26:58 +1000

    apply to event once, add phone number to token

--- utils/token_utilities.js
+++ utils/token_utilities.js
@@ -1,4 +1,5 @@
 const Token = require("../models/token")
+const User = require("../models/user")
 
 const getTokenKey = function(req){
     // req.body.lives = req.body.lives
@@ -10,21 +11,47 @@ let lastTokenLife = 0
 
 const redeemToken = function(token){
     console.log("THIS IS UTILS",token)
+    //this is for the token's last usage
     if (token.lives === 0 && token.valid === true){
         token.valid = false
+
         return Token.findByIdAndUpdate(token.id, token,{
             new: true,
             })
     }
+    // this is to deduct a life out of the token each time it's used
     if (token.lives > 0){
         token.lives = token.lives -= 1
+
         return Token.findByIdAndUpdate(token.id, token,{
             new: true,
             })
         }
+    // this is to not update if the token has expired
     if (token.lives === 0 && token.valid === false){
         return Token.findById(token)
     }
+}
+
+const acceptUser = function(token){
+    // let hasToken = await Token.find({"usedByPhone": `${phone.body.From}`})
+    // let user = await User.find({"phoneNumber": `${phone.body.From}`})
+
+
+    // let event = await Event.find({"applicants.phoneNumber":`${req.body.From}`})
+
+    // if (event !== undefined){
+
+    //     console.log(req)
+    //     let userDetails = {
+    //         username: req.user.username,
+    //         phoneNumber: req.body.From,
+    //     }
+    //     token.usedByPhone.push(userDetails)
+    //     token.save()
+    // }
+
+
 }
 
 module.exports = {getTokenKey, redeemToken}
